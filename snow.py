5d469441ff70120ccd221a7404b9825159bca82e
ahmetax <ahmetaksoy@gurmezin.com>
2016-03-23 01:55:32 +0200

    A simple house added to the scene

--- snow.py
+++ snow.py
@@ -1,37 +1,50 @@
+# -*- coding: utf-8 -*-
+#https://github.com/adamkdean/Python.Snowfall
+# Revision by: Ahmet aksoy - https://github.com/ahmetax/Python.Snowfall
 import random
 import sys
 import pygame
 import snowflake
-from snowflake import Snowflake
 from pygame.locals import * #@UnusedWildImport
+from snowflake import Snowflake
+
 
 class Game:
+    first_time=True
     def __init__(self, width, height, caption="Game"):
         # plethora of fields
         self.width = width
         self.height = height
         self.caption = caption
-        self.framerate = 60 # FPS        
+        self.framerate = 60 # FPS
         self.foreground_color = (255, 255, 255)
         self.background_color = (21, 26, 79)
         self.snowflakes = []
         self.snowflake_counter = 0
-        self.snowflake_frequency = 5
-        self.snowflake_size = 2
+        self.snowflake_frequency = 3
+        self.snowflake_size = 1
         self.snowflake_line = [height] * width # for collision detection
-        self.wind_chance = 1
-        self.wind_strength = 200
+        self.wind_chance = 3
+        self.wind_strength = 500
         self.show_text = True
         # and we're off!
         self.initialize()
         self.loop()
-        
+
     def initialize(self):
         pygame.init()
         pygame.display.set_caption(self.caption)
-        self.screen = pygame.display.set_mode((self.width, self.height))        
+        self.screen = pygame.display.set_mode((self.width, self.height))
         self.font = pygame.font.Font("fonts/visitor1.ttf", 20)
-    
+
+    # Draw a simple house. So snow can accumulate on top of its root also
+    def drawHouse(self,surface):
+        pygame.draw.rect(surface,(140,240,130), Rect((100,150), (130,50)))
+        if self.first_time:
+            for i in range(100,230):
+                self.snowflake_line[i]=150
+            self.first_time=False
+
     def loop(self):
         self.clock = pygame.time.Clock()
         while 1:
@@ -39,9 +52,9 @@ class Game:
             self.update(gametime)
             self.render(gametime)
             self.clock.tick(self.framerate)
-    
+
     def update(self, gametime):
-        
+
         # do we need to add more snow?
         if self.snowflake_counter > self.snowflake_frequency:
             self.snowflake_counter = 0
@@ -49,47 +62,48 @@ class Game:
             self.snowflakes.append(snowflake)
         else:
             self.snowflake_counter += 1
-            
+
         # what about some wind?
         w_chance = random.randint(0, 100)
         w_strength = 0
         if w_chance <= self.wind_chance:
             w_strength = random.randint(-self.wind_strength, self.wind_strength)
-            
+
         # let it snow, let it snow, let it snow
         for snowflake in self.snowflakes:
             if snowflake.enabled:
                 if w_strength != 0:
                     snowflake.wind = w_strength
                 snowflake.update(gametime, self.snowflake_line)
-        
+
         # update the other rubbish
         if self.show_text:
             self.fps_text = self.font.render("FPS: %d" % self.clock.get_fps(), 1, self.foreground_color)
             self.snowflake_text = self.font.render("Snowflakes: %d" % len(self.snowflakes), 1, self.foreground_color)
         self.handle_input(pygame.event.get())
-        
+
     def render(self, gametime):
         surface = pygame.Surface(self.screen.get_size())
         surface.convert()
         surface.fill(self.background_color)
-        
+        self.drawHouse(surface)
+
         for snowflake in self.snowflakes:
             snowflake.draw(surface)
-        
+
         if self.show_text:
             surface.blit(self.fps_text, (8, 6))
             surface.blit(self.snowflake_text, (8, 26))
         self.screen.blit(surface, (0, 0))
         pygame.display.flip()
-        
+
     def handle_input(self, events):
         for event in events:
-            if event.type == pygame.QUIT: 
+            if event.type == pygame.QUIT:
                 sys.exit()
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_d:
-                    self.show_text = False if self.show_text else True 
-                                      
-if __name__ == "__main__": 
-    game = Game(800, 600, "Snow: a sprinkling of test flakes")
+                    self.show_text = False if self.show_text else True
+
+if __name__ == "__main__":
+    game = Game(600, 200, "Snow: a sprinkling of test flakes")
--- snowflake.py
+++ snowflake.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import random
 import pygame.gfxdraw
 from pygame.locals import * #@UnusedWildImport
@@ -9,11 +10,11 @@ class Snowflake:
         self.height = height
         self.radius = radius
         self.color = color
-        self.gravity = random.uniform(5,15)
+        self.gravity = random.uniform(15,35)
         self.pos = [random.randint(0, self.width), 0]
         self.enabled = True
-        self.wind = 0        
-        
+        self.wind = 0
+
     def update(self, gametime, snowflake_line):
         if self.enabled:
             speed = 1 / float(gametime)
@@ -23,27 +24,27 @@ class Snowflake:
                 self.enabled = False
             if self.wind != 0:
                 w = (self.gravity * speed)/8
-                if self.wind < 0: w = -w                
+                if self.wind < 0: w = -w
                 self.pos[0] += w
                 self.wind -= w
-            
-    def collision_check(self, snowflake_line):        
+
+    def collision_check(self, snowflake_line):
         # check if it's hit the resting snow
         x = int(self.pos[0])
         y = self.pos[1]
         r = self.radius
-        points = snowflake_line[x-r:x+r]        
+        points = snowflake_line[x-r:x+r]
         for p in points:
             if y + r >= p:
                 for i in range(x-r, x+r):
                     if i >= 0 and i < self.width:
                         snowflake_line[i] = y
                 return True
-                    
+
         # check if it's on the bottom of the screen
-        if self.pos[1] >= self.height: return True                 
+        if self.pos[1] >= self.height: return True
         else: return False
-    
+
     def draw(self, surface):
-        pygame.gfxdraw.filled_circle(surface, int(self.pos[0]), int(self.pos[1]), self.radius, self.color)        
+        pygame.gfxdraw.filled_circle(surface, int(self.pos[0]), int(self.pos[1]), self.radius, self.color)
         pygame.gfxdraw.aacircle(surface, int(self.pos[0]), int(self.pos[1]), self.radius, self.color)
